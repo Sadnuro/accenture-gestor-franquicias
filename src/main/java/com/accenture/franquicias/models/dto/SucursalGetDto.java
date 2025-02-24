1a4aa348d454924e46e6dd51e36a5b0c50f7cab2
Sadith Nuñez <sadnr0213@gmail.com>
2025-02-23 19:40:39 -0500

    Sucursal: Se eliminan imports no usados

--- src/main/java/com/accenture/franquicias/models/dto/SucursalGetDto.java
+++ src/main/java/com/accenture/franquicias/models/dto/SucursalGetDto.java
@@ -1,9 +1,5 @@
 package com.accenture.franquicias.models.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
